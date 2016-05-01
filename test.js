0311c223fd690667ce276eef66d122052e556c0c
David Brownman <beamneocube@gmail.com>
2016-05-01 16:12:25 -0700

    mocha tests

--- test.js
+++ test.js
@@ -1,11 +1,38 @@
 'use strict'
+/* globals commands describe beforeEach it */
 
-const test = require('tape')
+const expect = require('chai').expect
+const nock = require('nock')
+const mock_fs = require('mock-fs')
 const rewire = require('rewire')
 
 const merge = require('./util/merge')
 const file = rewire('./util/file')
 
+const cli = require('heroku-cli-util')
+const commands = require('./index').commands
+
+// HELPERS
+function fetch_cmd (name) {
+  return commands.find((c) => c.command === name)
+}
+
+function setup () {
+  // no actual requets
+  nock.disableNetConnect()
+  cli.mockConsole()
+  cli.raiseErrors = true
+}
+
+function default_fs () {
+  // this is so I can setup without affecting other tests
+  return {
+    '.env': local_file,
+    'other.txt': local_file
+  }
+}
+
+// FIXTURES
 let remote = {
   NODE_ENV: 'production',
   NAME: 'david',
@@ -17,6 +44,7 @@ let local = {
   SOURCE: 'local',
   DB_STRING: 'mongo://blah@thing.mongo.thing.com:4567'
 }
+let local_file = '#comment\nNODE_ENV=test\nSOURCE=local\nDB_STRING=mongo://blah@thing.mongo.thing.com:4567\n'
 
 let remote_win = {
   NODE_ENV: 'production',
@@ -37,41 +65,74 @@ let sample_file = file.__get__('header') + 'NAME="david"\n\n#this is a comment!\
 let clean_sample_file = file.__get__('header') + 'NAME="david"\nCITY="boulder"\n'
 let sample_obj = {NAME: 'david', CITY: 'boulder'}
 
-test('Merging', (t) => {
-  t.deepEqual(remote_win,
-    merge(local, remote, {}),
-    'Remote should overwrite local')
-  t.deepEqual(local_win,
-    merge(remote, local, {}),
-    'Local should overwrite remote')
+// TESTS
+setup()
 
-  t.deepEqual(remote_win,
-    merge(remote, local, {overwrite: true}),
-    'Remote should overwrite local w/ overwrite')
-  t.deepEqual(local_win,
-    merge(local, remote, {overwrite: true}),
-    'Local should overwrite remote w/overwrite')
+describe('Merging', () => {
+  it('should overwrite local with remote', () => {
+    expect(merge(local, remote, {})).to.deep.equal(remote_win)
+  })
+  it('should overwrite remote with local', () => {
+    expect(merge(remote, local, {})).to.deep.equal(local_win)
+  })
 
-  t.end()
+  it('should overwrite local with remote w/override', () => {
+    expect(merge(remote, local, {overwrite: true})).to.deep.equal(remote_win)
+  })
+
+  it('should overwrite remote with local w/override', () => {
+    expect(merge(local, remote, {overwrite: true})).to.deep.equal(local_win)
+  })
 })
 
-test('Reading', (t) => {
-  t.deepEqual(file.read('asdf'),
-    {},
-    'Should return empty object from non-existant file')
+// describe('Reading', (t) => {
+//   it.deepEqual(file.read('asdf'),
+//     {},
+//     'Should return empty object from non-existant file')
 
-  t.end()
-})
+//   it.deepEqual(file.read('.env'),
+//     local,
+//     'Should read a local file')
 
-test('Transforming', (t) => {
-  t.deepEqual(file.__get__('obj_from_file_format')(sample_file),
-    sample_obj,
-    'Should dncode files correctly')
+// })
 
-  // this could get weird since object key order isn't garunteed, but it's probably ifne
-  t.deepEqual(file.__get__('obj_to_file_format')(sample_obj),
-    clean_sample_file,
-    'Should decodes file correctly')
+// describe('Transforming', (t) => {
+//   it.deepEqual(file.__get__('obj_from_file_format')(sample_file),
+//     sample_obj,
+//     'Should dncode files correctly')
 
-  t.end()
+//   // this could get weird since object key order isn't garunteed, but it's probably ifne
+//   it.deepEqual(file.__get__('obj_to_file_format')(sample_obj),
+//     clean_sample_file,
+//     'Should decodes file correctly')
+
+// })
+
+// describe('Pushing', {skip: true}, (t) => {
+//   let api_get = nock('https://api.heroku.com:443')
+//     .get('/apps/test/config-vars')
+//     .reply(200, remote)
+
+//   let api_patch = nock('https://api.heroku.com:443')
+//     .patch('/apps/test/config-vars')
+//     .reply(200, remote_win)
+
+//   let cmd = fetch_cmd('mypush')
+// })
+
+describe('Pulling', () => {
+  let fetch_configs = nock('https://api.heroku.com:443')
+    .get('/apps/test/config-vars')
+    .reply(200, remote)
+  mock_fs(default_fs())
+
+  it('should correctly pull configs', () => {
+    let cmd = fetch_cmd('mypull')
+    cmd.run({flags: {}, app: 'test'}).then((res) => {
+      console.log(res)
+
+      expect(true).to.equal(true)
+      fetch_configs.done()
+    })
+  })
 })
